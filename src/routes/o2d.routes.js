3f8756b3f8c50620cec0b2616e5dd8eeba7e6b3c
vinayakenterprises <seniordeveloper@vinayak-enterprises.com>
2026-08-24 14:06:41 +0530

    created: /interest-note-details-from-tally

--- src/routes/o2d.routes.js
+++ src/routes/o2d.routes.js
@@ -128,6 +128,7 @@ router.post('/update-overdue-summary-report-information', authMiddleware, o2dCon
 router.post('/test-api-for-tally-integration', o2dController.testApiForTallyIntegration);
 router.post('/so-orders-from-tally', documentUpload, o2dController.receiveSoOrdersFromTally);
 router.post('/invoice-details-from-tally', documentUpload, o2dController.receiveInvoiceDetailsFromTally);
+router.post('/interest-note-details-from-tally', documentUpload, o2dController.receiveInterestNoteDetailsFromTally);
 
 // this api is used to upload the pdf for the invoice
 router.post(
