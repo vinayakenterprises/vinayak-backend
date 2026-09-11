dbc0dccb203fa18545378e5a21f8524e8b81aa06
vinayakenterprises <seniordeveloper@vinayak-enterprises.com>
2026-09-11 14:09:24 +0530

    added remark functionality in - approve-credit-limit-exceeded-sale

--- src/services/o2d.service.js
+++ src/services/o2d.service.js
@@ -707,7 +707,7 @@ class O2dService {
 
   async approveCreditLimitExceededSale(body, userId) {
     try {
-      const { order_id, credit_limit_request_approval_status } = body;
+      const { order_id, credit_limit_request_approval_status, remark } = body;
 
       if (!order_id) {
         throw new Error("Order ID is required");
@@ -825,7 +825,15 @@ class O2dService {
         const approveQuery = `
         UPDATE sales_orders
         SET credit_limit_info = COALESCE(credit_limit_info, '{}'::jsonb)
-            || jsonb_build_object('credit_limit_request_approved_at', now(), 'credit_limit_request_approval_status', true),
+            || jsonb_build_object(
+              'credit_limit_request_approved_at', now(),
+              'credit_limit_request_approval_status', true
+            )
+            || CASE
+                WHEN $3::text IS NOT NULL AND TRIM($3::text) <> ''
+                THEN jsonb_build_object('remark', $3::text)
+                ELSE '{}'::jsonb
+              END,
             order_status = $2
         WHERE id = $1
         RETURNING *;
@@ -834,6 +842,7 @@ class O2dService {
         const { rows } = await pool.query(approveQuery, [
           order_id,
           soGenerationStage,
+          remark, 
         ]);
 
         // sendNotificationToCrm(order_id);
@@ -870,7 +879,15 @@ class O2dService {
         const rejectQuery = `
         UPDATE sales_orders
         SET credit_limit_info = COALESCE(credit_limit_info, '{}'::jsonb)
-            || jsonb_build_object('credit_limit_request_approved_at', now(), 'credit_limit_request_approval_status', false),
+            || jsonb_build_object(
+              'credit_limit_request_approved_at', now(),
+              'credit_limit_request_approval_status', false
+            )
+            || CASE
+                WHEN $3::text IS NOT NULL AND TRIM($3::text) <> ''
+                THEN jsonb_build_object('remark', $3::text)
+                ELSE '{}'::jsonb
+              END,
             order_status = $2
         WHERE id = $1
         RETURNING *;
@@ -879,6 +896,7 @@ class O2dService {
         const { rows } = await pool.query(rejectQuery, [
           order_id,
           soGenerationStage,
+          remark,
         ]);
 
         // sendNotificationToCrm(order_id);
